b11f5fb3f7976735609bbe32fe750fd209f39fe6
ogison <meyo.ogs9@gmail.com>
2024-11-19 23:23:49 +0900

    Updated: real-sns-udemy adding CRUD

--- real-sns-udemy/routes/users.js
+++ real-sns-udemy/routes/users.js
@@ -1,3 +1,41 @@
 const router = require("express").Router();
+const User = require("../models/User");
+
+// CRUD
+// update User
+router.post("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+
+      return res.status(200).json("ユーザー情報が登録されました");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res
+      .status(403)
+      .json("あなたは自分のアカウントの時だけ情報を更新できます");
+  }
+});
+
+// Delete User
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+
+      return res.status(200).json("ユーザー情報が削除されました");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res
+      .status(403)
+      .json("あなたは自分のアカウントの時だけ情報を更新できます");
+  }
+});
 
 module.exports = router;
