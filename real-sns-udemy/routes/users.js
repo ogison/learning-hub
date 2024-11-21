e351fd36b7809ab8d1ce51f9ac553a9e2f604d66
ogison <meyo.ogs9@gmail.com>
2024-11-21 23:52:15 +0900

    Updated: real-sns-udemy adding follow action

--- real-sns-udemy/routes/users.js
+++ real-sns-udemy/routes/users.js
@@ -34,7 +34,50 @@ router.delete("/:id", async (req, res) => {
   } else {
     return res
       .status(403)
-      .json("あなたは自分のアカウントの時だけ情報を更新できます");
+      .json("あなたは自分のアカウントの時だけ情報を削除できます");
+  }
+});
+
+// Get User
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    return res.status(200).json(other);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+// Follow User
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      // you can follow if they are not in the followers list
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({
+          $push: {
+            followers: req.body.userId,
+          },
+        });
+        await currentUser.updateOne({
+          $push: {
+            followings: req.params.userId,
+          },
+        });
+        return res.status(200).json("フォローに成功しました。");
+      } else {
+        return res
+          .status(403)
+          .json("あなたはすでにこのユーザーをフォローしています。");
+      }
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(500).json("自分自身をフォローできません。");
   }
 });
 
