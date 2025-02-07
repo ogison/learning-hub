10a9a7b54d7b3ac13107e88e60021156f1cb37e3
ogison <meyo.ogs9@gmail.com>
2025-02-08 08:49:33 +0900

    Updated: remix-beginner add layout

--- remix-beginner/my-react-router-app/app/routes.ts
+++ remix-beginner/my-react-router-app/app/routes.ts
@@ -1,6 +1,10 @@
 import type { RouteConfig } from "@react-router/dev/routes";
-import { route } from "@react-router/dev/routes";
+import { index, layout, route } from "@react-router/dev/routes";
 
 export default [
-  route("contacts/:contactId", "routes/contact.tsx"),
+  layout("layouts/sidebar.tsx", [
+    index("routes/home.tsx"),
+    route("contacts/:contactId", "routes/contact.tsx"),
+  ]),
+  route("about", "routes/about.tsx"),
 ] satisfies RouteConfig;
