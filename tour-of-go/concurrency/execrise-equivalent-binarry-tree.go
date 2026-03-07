e82b0ffd5bc9ff65ac3390bac4c7b3d19c54dd7a
ogison <meyo.ogs9@gmail.com>
2026-03-07 15:39:04 +0900

    Updated: tour of go (#9)

---
+++ tour-of-go/concurrency/execrise-equivalent-binarry-tree.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+
+	"golang.org/x/tour/tree"
+)
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, ch chan int) {
+	for t != nil {
+		Walk(t.Left, ch)
+		ch <- t.Value
+		Walk(t.Right, ch)
+	}
+	return
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+	return reflect.DeepEqual(<-ch1, <-ch2)
+}
+
+func main() {
+	fmt.Println(Same(tree.New(1), tree.New(1)))
+	fmt.Println(Same(tree.New(1), tree.New(2)))
+}
